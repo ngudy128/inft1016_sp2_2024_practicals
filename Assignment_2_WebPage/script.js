91041ddb98dd1fbfff9df0662d9eb0cbef700458
ngudy128 <ngudy128@mymail.unisa.edu.au>
2024-06-07 15:53:50 +0930

    Add buttons

---
+++ Assignment_2_WebPage/script.js
@@ -0,0 +1,14 @@
+function change_image_1() {
+    var img = document.getElementById("image")
+    var texteElement = document.getElementById("text")
+    img.src = "https://www.shutterstock.com/shutterstock/photos/1226807464/display_1500/stock-photo-dices-with-teamwork-idea-business-1226807464.jpg";
+    texteElement.innerHTML = "Dices with Teamwork Ideas Business. A great picture to show how our ideas are generated through many deeep thoughts";
+
+}
+
+function change_image_2() {
+    var img = document.getElementById("image")
+    var texteElement = document.getElementById("text")
+    img.src = "https://www.shutterstock.com/shutterstock/photos/2286643233/display_1500/stock-photo--goals-of-business-or-life-wooden-cubes-with-and-goal-icon-on-smart-background-starting-2286643233.jpg";
+    texteElement.innerHTML = "2024 goals of business or life";
+}
